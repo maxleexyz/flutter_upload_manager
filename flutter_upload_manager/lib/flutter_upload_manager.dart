c9a30ba45a6f0861f74d6cb31a9785f090953914
Alexander.Li <superpowerlee@gmail.com>
2021-03-05 09:42:20 +0800

    完成

---
+++ flutter_upload_manager/lib/flutter_upload_manager.dart
@@ -0,0 +1,234 @@
+library flutter_upload_manager;
+
+import 'dart:async';
+import 'dart:io';
+
+class UploadException extends Error {
+  String errMsg() => 'Upload Faild';
+}
+
+const int DEFAULT_CHUNK_SIZE = 93;
+//const int DEFAULT_CHUNK_SIZE = 1024 * 1024 * 5;
+
+class ChunkState {
+  static const IdKey = 'Id';
+  static const StartIdxKey = 'startIdx';
+  static const EndIdxKey = 'endIdx';
+  static const StateKey = 'state';
+  static const EtagKey = 'etag';
+
+  /// chunkId
+  int id;
+
+  /// range start of file
+  int startIdx;
+
+  /// range end of file
+  int endIdx;
+
+  /// upload state 0=uploading 1=success 2
+  int state;
+
+  /// etag from upload result
+  String etag;
+
+  /// save object as a dictionary
+  Map asMap() => {
+        IdKey: id,
+        StartIdxKey: startIdx,
+        EndIdxKey: endIdx,
+        StateKey: state,
+        EtagKey: etag
+      };
+
+  /// Constructor for new
+  ChunkState(this.id, this.startIdx, this.endIdx, this.state);
+
+  /// Constructor for instance from storage
+  ChunkState.fromMap(Map map) {
+    this.id = map[IdKey];
+    this.startIdx = map[StartIdxKey];
+    this.endIdx = map[EndIdxKey];
+    this.state = map[StateKey];
+    this.etag = map[EtagKey];
+  }
+  @override
+  String toString() =>
+      "id:$id from:$startIdx to:$endIdx state:$state of etag:$etag";
+}
+
+class UpState {
+  static const UploadIdKey = 'uploadId';
+  static const FilepathKey = 'filepath';
+  static const FilesizeKey = 'filesize';
+  static const ChunksKey = 'chunks';
+  static const SuccessCountKey = 'successCount';
+
+  /// unique id of
+  String uploadId;
+
+  /// upload file path
+  String filePath;
+
+  /// file size used to caculate chunks
+  int fileSize;
+
+  /// upload chunks
+  List<ChunkState> chunks;
+
+  /// success chunks count
+  int successCount;
+
+  /// Constructor for new
+  UpState(this.uploadId, this.filePath, this.fileSize, this.successCount) {
+    assert(this.fileSize <= DEFAULT_CHUNK_SIZE * 100);
+    this.chunks = <ChunkState>[];
+    for (var i = 0; i < 300; i++) {
+      final startIdx = i * DEFAULT_CHUNK_SIZE;
+      var endIdx = (i + 1) * DEFAULT_CHUNK_SIZE;
+      if (endIdx > fileSize - 1) {
+        endIdx = fileSize - 1;
+      }
+      this.chunks.add(new ChunkState(i + 1, startIdx, endIdx, 0));
+      if (endIdx == fileSize - 1) {
+        // be sure reach the end of file, quit loop
+        break;
+      }
+    }
+  }
+
+  /// Constructor for instance from storage
+  UpState.fromMap(Map map) {
+    this.uploadId = map[UploadIdKey];
+    this.filePath = map[FilepathKey];
+    this.fileSize = map[FilesizeKey];
+    for (final ck in map[ChunksKey]) {
+      this.chunks.add(ChunkState.fromMap(ck));
+    }
+    this.successCount = map[SuccessCountKey];
+  }
+  @override
+  String toString() =>
+      "$filePath($fileSize) chunks:${chunks.map((c) => c.toString()).join('\n')} successCount:$successCount";
+}
+
+/// Delegate for implement state storage
+abstract class StateDelegate {
+  Future saveState(UpState state);
+  UpState loadByPath(String filePath);
+  Future removeState(String filePath);
+}
+
+/// Delegate for implement upload
+abstract class UploadDelegate {
+  initUpload(UpState state);
+  Future<UpState> directUpload(UpState state, List<int> fileData);
+  Future<UpState> initPartialUpload(UpState state);
+  Future<String> uploadPart(UpState state, int idx, List<int> chunkData);
+  Future<UpState> cpmletePart(UpState state);
+  updatePercentage(int total, int success);
+  onFinished(UpState state);
+}
+
+/// Manager
+class UpManager {
+  /// executor
+  UploadDelegate upExecutor;
+
+  /// state
+  StateDelegate stateStorage;
+
+  /// Constructor
+  UpManager(this.upExecutor, this.stateStorage);
+
+  Future<List<int>> loadFile(filePath) async {
+    final file = new File(filePath);
+    return (await file.readAsBytes()).toList();
+  }
+
+  Future upfile(String filePath, int fileSize) async {
+    UpState state = stateStorage.loadByPath(filePath);
+    if (state != null && state.successCount == state.chunks.length) {
+      // if have a old state, check if need reupload
+      await _processOldState(state);
+    } else {
+      final fileData = await loadFile(filePath);
+      if (state == null) {
+        state = UpState('', filePath, fileSize, 0);
+        upExecutor.initUpload(state);
+        if (state.chunks.length < 2) {
+          // if single chunk
+          await _processOneChunk(state, fileData, filePath);
+        } else {
+          await _processMultiChunk(state, filePath, fileData);
+        }
+      } else {
+        // 端点续传
+        await _processBrokenState(state, fileData, filePath);
+      }
+    }
+  }
+
+  Future _processBrokenState(
+      UpState state, List<int> fileData, String filePath) async {
+    assert(state.uploadId.isNotEmpty);
+    final ps = state.chunks
+        .map((i) => i)
+        .where((chunkState) => chunkState.state < 1)
+        .map((e) => processUpPart(state, e.id - 1, fileData));
+    await Future.wait(ps);
+    await _checkResult(state, filePath);
+  }
+
+  Future _processMultiChunk(
+      UpState state, String filePath, List<int> fileData) async {
+    state = await upExecutor.initPartialUpload(state);
+    final chunkIdxList = new List<int>.generate(state.chunks.length, (i) => i);
+    // wait for each chunk uploaded
+    await Future.wait(
+        chunkIdxList.map((cid) => processUpPart(state, cid, fileData)));
+    await _checkResult(state, filePath);
+  }
+
+  Future _checkResult(UpState state, String filePath) async {
+    if (state.chunks.map((st) => st.state).reduce((v1, v2) => v1 + v2) !=
+        state.chunks.length) {
+      // if success count not equal to total means faild
+      await stateStorage.saveState(state);
+      upExecutor.onFinished(state);
+    } else {
+      await stateStorage.removeState(filePath);
+      upExecutor.onFinished(state);
+    }
+  }
+
+  Future processUpPart(UpState state, int chunkIdx, List<int> fileData) async {
+    final chunkState = state.chunks[chunkIdx];
+    List<int> chunkData =
+        fileData.sublist(chunkState.startIdx, chunkState.endIdx);
+    final etag = await upExecutor.uploadPart(state, chunkIdx, chunkData);
+    if (etag.isNotEmpty) {
+      chunkState.state = 1;
+      chunkState.etag = etag;
+      state.successCount += 1;
+      upExecutor.updatePercentage(state.successCount, state.chunks.length);
+    } else {
+      throw new UploadException();
+    }
+  }
+
+  Future _processOneChunk(
+      UpState state, List<int> fileData, String filePath) async {
+    state = await upExecutor.directUpload(state, fileData);
+    if (state.successCount > 0) {
+      await stateStorage.removeState(filePath);
+      assert(state != null);
+      upExecutor.onFinished(state);
+    }
+  }
+
+  Future _processOldState(UpState state) async {
+    // success direct and remove state
+    await stateStorage.removeState(state.filePath);
+  }
+}
---
+++ flutter_upload_manager/test/flutter_upload_manager_test.dart
@@ -0,0 +1,131 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_upload_manager/flutter_upload_manager.dart';
+
+class Storage implements StateDelegate {
+  Storage();
+  UpState old;
+  Storage.fromOldState(UpState state) {
+    old = state;
+  }
+  @override
+  UpState loadByPath(String filePath) {
+    // TODO: implement loadByPath
+    return old;
+  }
+
+  @override
+  Future removeState(String filePath) {
+    // TODO: implement removeState
+    print('do remove state:$filePath');
+  }
+
+  @override
+  Future saveState(UpState state) {
+    // TODO: implement saveState
+    print('do save state:$state');
+  }
+}
+
+class Uploader implements UploadDelegate {
+  @override
+  Future<UpState> cpmletePart(UpState state) async {
+    // TODO: implement cpmletePart
+    print("complete");
+    return state;
+  }
+
+  @override
+  Future<UpState> directUpload(UpState state, List<int> fileData) async {
+    // TODO: implement directUpload
+    print("will upload: $state");
+    print("data len:${fileData.length}");
+    await Future.delayed(Duration(seconds: 1));
+    print('uploaded');
+    state.chunks[0].state = 1;
+    state.successCount += 1;
+    return state;
+  }
+
+  @override
+  Future<UpState> initPartialUpload(UpState state) async {
+    // TODO: implement initPartialUpload
+    await Future.delayed(Duration(seconds: 1));
+    print("part upload inited");
+    return state;
+  }
+
+  @override
+  initUpload(UpState state) {
+    // TODO: implement initUpload
+    print('init upload:$state');
+    return state;
+  }
+
+  @override
+  onFinished(UpState state) {
+    // TODO: implement onFinished
+    print("upload finished with state:$state");
+  }
+
+  @override
+  updatePercentage(int success, int total) {
+    // TODO: implement updatePercentage
+    print("$success/$total");
+  }
+
+  @override
+  Future<String> uploadPart(UpState state, int idx, List<int> chunkData) async {
+    // TODO: implement uploadPart
+    await Future.delayed(Duration(seconds: 1));
+    print("idx $idx uploaded ${chunkData.length} bytes");
+    return '${new DateTime.now().millisecondsSinceEpoch}';
+  }
+}
+
+void main() {
+  test('new state', () {
+    UpState state = new UpState('', 'path', 100, 0);
+    final list = new List<int>.generate(100, (i) => i + 1);
+    var splitList = [];
+    for (var i = 0; i < state.chunks.length; i++) {
+      final chunkstate = state.chunks[i];
+      splitList.add(list.sublist(chunkstate.startIdx, chunkstate.endIdx));
+    }
+    for (var j = 0; j < splitList.length - 1; j++) {
+      final r1 = splitList[j];
+      final r2 = splitList[j + 1];
+      expect(r1.last + 1, r2.first);
+    }
+  });
+  test("single upload", () async {
+    final stateStorage = new Storage();
+    final executor = new Uploader();
+    final manager = new UpManager(executor, stateStorage);
+    final state = await manager.upfile(
+        "/Users/alex/Projects/workspace/flutter_upload_manager/flutter_upload_manager/test/test_data.txt",
+        1070);
+  });
+
+  test("Multiple upload", () async {
+    final stateStorage = new Storage();
+    final executor = new Uploader();
+    final manager = new UpManager(executor, stateStorage);
+    final state = await manager.upfile(
+        "/Users/alex/Projects/workspace/flutter_upload_manager/flutter_upload_manager/test/test_data.txt",
+        1070);
+  });
+
+  test("Broken upload", () async {
+    final filePath =
+        "/Users/alex/Projects/workspace/flutter_upload_manager/flutter_upload_manager/test/test_data.txt";
+    final upState = new UpState("test_id", filePath, 1070, 3);
+    upState.chunks[0].state = 1;
+    upState.chunks[1].state = 1;
+    upState.chunks[2].state = 1;
+
+    final stateStorage = Storage.fromOldState(upState);
+    final executor = new Uploader();
+    final manager = new UpManager(executor, stateStorage);
+    final state = await manager.upfile(filePath, 1070);
+  });
+}
